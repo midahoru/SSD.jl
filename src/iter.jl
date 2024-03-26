167bf1393b531097fddfe6803e0b183d0a8f112d
midahoru <migueldhr@protonmail.com>
2024-03-26 10:27:53 -0400

    Small refractoring iter.jl

--- src/iter.jl
+++ src/iter.jl
@@ -6,12 +6,10 @@ function cutting_plane(data, params, status, ρ_h)
     q = 0
 
     while (ub-lb)/ub >= params.ϵ
-        println("Current LB= $lb ; UB = $ub")
         xq, yq, ρq, Rq, wq, lb = model_cuts(data, params, status, ρ_h)
-        println("Optimal LB= $lb")
 
         ub = calc_ub(lb, ub, ρq, Rq, wq, data)
-        
+
         ρ_h = cat(ρ_h, ρq, dims=3)
         q+=1
         println("Iter $q: LB= $lb ; UB = $ub")
